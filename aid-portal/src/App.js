92db53ee165488b8c49a005ba6de0da7f755d6f8
karansingh01 <karanss@stud.ntnu.no>
2022-02-16 21:03:06 +0100

    connected pages and added MUI

--- aid-portal/src/App.js
+++ aid-portal/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes ,Route,  BrowserRouter } from 'react-router-dom';
+
+import Login from './pages/login/login';
+import Profile from './pages/profile/profile';
+import Home from './pages/homepage/home';
+
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
   );
 }
+  
 
 export default App;
