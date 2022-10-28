8de4875b7e83dc19f7266db2bbf5ed92b16882b3
Silje Eriksen <silje@dhcp-10-24-6-213.wlan.ntnu.no>
2022-10-28 13:38:05 +0200

    Added interface for experienceReport, User, project

---
+++ iug/src/models/experienceReport.ts
@@ -0,0 +1,8 @@
+
+export interface experienceReport{
+    id: string;
+    title: string;
+    description: string;
+    projectId: string;
+    studentId: string;
+}
--- iug/src/models/user.ts
+++ iug/src/models/user.ts
@@ -1,11 +1,16 @@
 /*
 Type used for an standard and admin user
 */
-export interface UserProfile {
-    email: string;
-    firstName: string;
-    lastName: boolean;
-    phoneNumber: string;
-    age: number;
-    gender: string;
-  }
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  academicRole?: string;
+  institute?: string;
+  university: string;
+  picture?: string;
+  contributionIds?: Map<string,string>;
+  role: string;
+  phoneNumber?: string;
+}
