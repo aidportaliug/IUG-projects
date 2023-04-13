3fa0b3531ff0f0477593edaa7e78025d4ec4211e
Kristian <kamkarls@stud.ntnu.no>
2023-04-13 11:53:07 +0200

    navbar3

---
+++ iug/src/components/Navbar/Navlogo.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import IUGlogo from "../../images/logo.png";
+
+const NavLogo = () => {
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+  return (
+    <button
+      onClick={() => handleLogoClick()}
+      style={{
+        border: "none",
+        outline: "none",
+        padding: 0,
+        backgroundColor: "transparent",
+        cursor: "pointer",
+      }}
+    >
+      <img src={IUGlogo} alt="IUGLogo" style={{ width: "12em" }} />
+    </button>
+  );
+};
+
+export default NavLogo;
