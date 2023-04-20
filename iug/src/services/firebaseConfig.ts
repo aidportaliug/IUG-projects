193303de29e2587c7adbbf7a38e8f189086fd9bd
Silje Eriksen <silje@MacBook-Air-5.local>
2023-04-20 12:06:44 +0200

    Initial setup

--- iug/src/services/firebaseConfig.ts
+++ iug/src/services/firebaseConfig.ts
@@ -1,7 +1,8 @@
-import { getAuth } from "firebase/auth";
 import firebase from "firebase/compat/app";
-import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,11 +13,20 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: `G-${process.env.REACT_APP_MEASUREMENT_ID}`,
 };
+
 // console.log("HELLU:" + firebaseConfig.apiKey);
 // console.log("HELLU:" + firebaseConfig.authDomain);
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+const functions = getFunctions(app);
+if (window.location.hostname === "localhost") {
+  console.log("localhost detected!");
+  connectFirestoreEmulator(db, "localhost", 8080);
+  const auth = getAuth();
+  connectAuthEmulator(auth, "http://localhost:9099");
+  connectFunctionsEmulator(functions, "localhost", 5001);
+}
+export const auth = getAuth(app);
 export const storage = getStorage(app);
 export default app;
