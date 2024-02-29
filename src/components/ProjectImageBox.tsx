79f3991941ae09d8a9a509ee6a95c671135d5eed
andreas <andreas@kissmehl.de>
2024-02-29 18:02:14 +0100

    linting...

--- src/components/ProjectImageBox.tsx
+++ src/components/ProjectImageBox.tsx
@@ -1,17 +1,20 @@
-import React, {ReactElement} from "react";
-import "../styles/ProjectImageBox.css"
+import React, { ReactElement } from "react";
+import "../styles/ProjectImageBox.css";
 
 interface projectImageBoxProp {
-    source: string;
-    altText: string;
+  source: string;
+  altText: string;
 }
 
-const ProjectImageBox = ({source, altText}: projectImageBoxProp): ReactElement => {
-    return (
-        <div className="projectImageBox">
-            <img src={source} alt={altText}/>
-        </div>
-    );
-}
+const ProjectImageBox = ({
+  source,
+  altText,
+}: projectImageBoxProp): ReactElement => {
+  return (
+    <div className="projectImageBox">
+      <img src={source} alt={altText} />
+    </div>
+  );
+};
 
 export default ProjectImageBox;
--- src/pages/ProjectDetailsPage.tsx
+++ src/pages/ProjectDetailsPage.tsx
@@ -10,7 +10,6 @@ import Trax_Ghana from "./../images/Trax_Ghana.png";
 import ProjectImageBox from "../components/ProjectImageBox";
 import Meta from "../components/Meta";
 
-
 const ProjectDetailsPage: React.FC = () => {
   const name = "Trond Are Øritsland";
   const professionalTitle = "Førsteamanuensis";
@@ -32,35 +31,35 @@ const ProjectDetailsPage: React.FC = () => {
   }, [id]);
   if (project != null) {
     return (
-        <>
-      <Meta title={"Your profile"}></Meta>
-      <div className="projectDetailoutline">
-        <div className="Title">{project?.title}</div>
-        <ProjectImageBox source={imageIcon} altText={"String"} />
-        <Grid container spacing={10}>
-          <Grid item xs={8}>
-            <img className="projectDetailImage" src={imageIcon} alt="" />
+      <>
+        <Meta title={"Your profile"}></Meta>
+        <div className="projectDetailoutline">
+          <div className="Title">{project?.title}</div>
+          <ProjectImageBox source={imageIcon} altText={"String"} />
+          <Grid container spacing={10}>
+            <Grid item xs={8}>
+              <img className="projectDetailImage" src={imageIcon} alt="" />
+            </Grid>
+            <Grid item xs={4}>
+              <ProfessorCard
+                name={name}
+                professionalTitle={professionalTitle}
+                institute={institute}
+                email={email}
+                phoneNumber={phoneNumber}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <ProfessorCard
-              name={name}
-              professionalTitle={professionalTitle}
-              institute={institute}
-              email={email}
-              phoneNumber={phoneNumber}
-            />
-          </Grid>
-        </Grid>
-        <InformationBox
-          deadline={project.deadline.toDate()}
-          location={project.location}
-          duration={project.duration}
-          studyField={project.studyField}
-        ></InformationBox>
-        <div style={{ width: "70%", fontSize: "15px" }}>
-          {project.description}
+          <InformationBox
+            deadline={project.deadline.toDate()}
+            location={project.location}
+            duration={project.duration}
+            studyField={project.studyField}
+          ></InformationBox>
+          <div style={{ width: "70%", fontSize: "15px" }}>
+            {project.description}
+          </div>
         </div>
-      </div>
       </>
     );
   }
