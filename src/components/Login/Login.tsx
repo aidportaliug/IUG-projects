6dfcde0275198b8d419e6ad71f6693dc01cf5234
Sigve Kulsrud <sigvefk@ntnu.no>
2024-11-08 15:15:29 +0100

    Fixed linting error in Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Container, Box, Typography, TextField, FormControlLabel, Checkbox, Button, Divider } from '@mui/material';
 import logIn from '../../services/auth';
 
