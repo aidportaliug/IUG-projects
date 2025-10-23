c44dacec51be1327f37d61cb3fadda8df82bee5e
Sigve Kulsrud <sigvefk@ntnu.no>
2025-10-23 16:49:43 +0200

    Small fixes

--- src/pages/uploadExperienceReport/UploadExperienceReport.tsx
+++ src/pages/uploadExperienceReport/UploadExperienceReport.tsx
@@ -24,6 +24,7 @@ const UploadExperienceReport: React.FC = () => {
     }
   }, [customUser, user, userUpdated]);
 
+  //if (customUser !== null && customUser?.professor === false) {
   if (true) {
     return (
       <>
